c974f8c008d90a42c3b193a7cdb0e006aaf4969a
Cinthia Godoi <cinthiagodoi@hotmail.com>
2020-07-29 14:01:11 +0100

    final project

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const smallerCities = []
 
 const init = () => {
   writeStateFiles();
+  userInputState();
 }
 
 const readFile = async (file) => JSON.parse(await fs.readFile(file));
@@ -26,21 +27,18 @@ const writeStateFiles = async () => {
         uf: state.Sigla,
         quantity: data.cities.length,
       }
-
       stateInformation.push(statesData)
-     
       await fs.writeFile(`api/states/${state.Sigla}.json`, JSON.stringify(data));
     }
   } catch(err) {
     console.log(err)
   }
-  //userInputState();
-  //biggerCityName(stateInformation)
-  //smallerCityName(stateInformation)
-  //biggerStates(stateInformation);
+  biggerCityName(stateInformation);
+  smallerCityName(stateInformation);
+  biggerStates(stateInformation);
   smallerStates(stateInformation);
-  
 }
+
 const filterState = (val, state) => { return val.Estado === state.ID }
 
 const readInterface = readline.createInterface({
@@ -50,29 +48,29 @@ const readInterface = readline.createInterface({
 
 const stateFilter = async (state) => {
   try {
-    const readStateFile = await readFile((`${api}/${state}.json`))
-    console.log(state, ' - ', readStateFile.cities.length)
+    const readStateFile = await readFile((`${api}/${state}.json`));
+    console.log(state, ' - ', readStateFile.cities.length);
   
-    readInterface.close()
+    readInterface.close();
   } catch(err){
-    console.log(err)
-  }
-}
+    console.log(err);
+  };
+};
 
 const userInputState = () => {
   readInterface.question('Enter a state: ', input => {
-    let inputState = input.toUpperCase()
-    stateFilter(inputState)
-  })
-}
+    let inputState = input.toUpperCase();
+    stateFilter(inputState);
+  });
+};
 
 const biggerStates = (data) => {
   console.log(data.sort((x, y) => y.quantity - x.quantity).slice(0, 5))
-}
+};
 
 const smallerStates = (data) => {
   console.log(data.sort((x, y) => x.quantity - y.quantity).slice(0, 5))
-}
+};
 
 const biggerCityName = async (states) => {
   try{
@@ -99,14 +97,14 @@ const smallerCityName = async (states) => {
       const readCitiesFile = await readFile(`${api}/${state.uf}.json`);
       let alphabeticalOrder = ((readCitiesFile.cities).sort((a, b) => {
         return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
-      }))
+      }));
    
       const sortCity = (alphabeticalOrder.sort((x, y) => x.Nome.length - y.Nome.length).slice(0, 1))
       sortCity.map(city => {
         let result = {
           name: city.Nome,
           uf: state.uf
-        }
+        };
         smallerCities.push(result)
         return result
       });
@@ -120,14 +118,14 @@ const smallName = (smallerCities) => {
   
   let alphabeticalOrder = (smallerCities.sort((a, b) => {
     return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
-  }))
+  }));
   console.log(alphabeticalOrder.sort((x, y) => x.name.length - y.name.length).slice(0,1))
 }
 
 const bigName = (bigestCities) => {
   let alphabeticalOrder = (bigestCities.sort((a, b) => {
     return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
-  }))
+  }));
   console.log(alphabeticalOrder.sort((x, y) => y.name.length - x.name.length).slice(0,1))
 }
 
